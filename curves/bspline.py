6104e5a86b072477bbd9dadf0f42f6ebe16bc02c
ger <g.duran@me.com>
2017-08-22 13:18:09 -0500

    edit Bspline notebook and python file

--- curves/bspline.py
+++ curves/bspline.py
@@ -6,7 +6,7 @@ class BSpline(object):
         self.len_knots = len(knots)
         self.degree = degree
 
-    def eval(self, t, knot, degree=None):
+    def eval(self, t, knot, degree=None, out_bounds_eval=False):
         """
         Evaluate the B-Spline at a given knot and t in the
         range of knots. Optional to evaluate the bspline at
@@ -23,6 +23,10 @@ class BSpline(object):
         degree: int, optional
                 The degree of the B-Spline
 
+        out_bounds_eval:bool, optional
+                        If True, it returns 0 for bspline evaluation out
+                        of the possible bounds
+
         Returns
         -------
         float
@@ -33,11 +37,14 @@ class BSpline(object):
             degree = self.degree
 
         if knot + degree + 1  >= self.len_knots:
-            err = ("BSpline cannot be evaluated for "
-                   "{knot} + {degree} + 1 >= {lenkn}"
-                   .format(knot=knot, degree=degree,
-                   lenkn=self.len_knots))
-            raise IndexError(err)
+            if out_bounds_eval:
+                return 0
+            else:
+                err = ("BSpline cannot be evaluated for "
+                       "{knot} + {degree} + 1 >= {lenkn}"
+                       .format(knot=knot, degree=degree,
+                       lenkn=self.len_knots))
+                raise IndexError(err)
 
         if degree == 0:
             degree0_spline = lambda t, k: (self.knots[k] <= t < self.knots[k + 1]) * 1
@@ -88,5 +95,32 @@ class BSpline(object):
             return total_integration
 
 
-    def diff(self, t):
-        pass
+    def diff(self, t, knot):
+        """
+        Evaluate the derivative of the integral
+
+        Paramters
+        ---------
+        t:  float
+            The point at which the B-Spline will be evaluated
+
+        knot:  int
+            The index of the knot to reference the bspline
+
+
+        Returns
+        -------
+        float
+            The derivative of the B-Spline evaluated at t
+        """
+        degree_minus1 = self.degree - 1
+        # B_k^{(d-1)}(t)
+        B_k_dm1_t = self.eval(t, knot, degree=degree_minus1)
+        # B_{k+1}^{(d-1)}(t)
+        B_kp1_dm1_t = self.eval(t, knot+1, degree=degree_minus1)
+
+        term1 = self.degree / (self.knots[knot + self.degree] - self.knots[knot])
+        term2 = self.degree / (self.knots[knot + self.degree + 1] - self.knots[knot + 1])
+
+        return term1 * B_k_dm1_t - term2 * B_kp1_dm1_t
+
